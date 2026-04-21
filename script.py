6824701cf97421601f94dd938bf6b922fdb7a999
brbry-21 <abdelrahman.elbarbary21@gmail.com>
2026-04-21 04:44:56 +0200

    Update script.py

--- script.py
+++ script.py
@@ -25,7 +25,7 @@ def post_image(i):
         print("CHECK PATH:", image_path)
         print("EXISTS:", os.path.exists(image_path))
         
-    caption = f"#الأسماء_والصفات__{i-1}"
+    caption = f"#الأسماء_والصفات_{i-1}"
 
     url = f"https://graph.facebook.com/{PAGE_ID}/photos"
 
